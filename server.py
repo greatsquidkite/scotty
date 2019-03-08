67ea387a04856a5d15e57531cf1083b0e684b9fc
pat <greatsquidkite@gmail.com>
2019-03-08 13:09:20 -0800

    Threading implemented, start and stop server functions working!

--- server.py
+++ server.py
@@ -9,27 +9,32 @@ SERVER_DIR = ""
 
 #TODO change server launch to threading
 
-def server():
-	print(dirpath.text())
-	if localOnly:
-		print("launching")
-		s = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
-	else:
-		exit()
-	thread = threading.Thread(target = s.serve_forever)
-	thread.daemon = True
+class ScottyServer():
+	def server(self, arg2):
+		print(arg2)
+		print(dirpath.text())
+		if localOnly:
+			print("launching")
+			self.s = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+		else:
+			exit()
+		self.thread = threading.Thread(target = self.s.serve_forever)
+		self.thread.daemon = True
+		self.thread.start()
+		print("success")
 
-def kill_server():
-	try:
-		s.shutdown()
-	except:
-		print("Server not running")
+	def kill_server(self):
+		try:
+			self.s.shutdown()
+		except:
+			print("Server not running")
 
 app = QApplication([])
 app.setStyle('Fusion')
 window = QWidget()
 layout = QVBoxLayout()
 # file = QFileDialog.getExistingDirectory('Select Directory')
+scotty = ScottyServer()
 
 #check for no remote access
 localOnly = QCheckBox('Local Only')
@@ -39,11 +44,11 @@ dirpath = QLineEdit()
 
 #connect start button to server function
 run_btn = QPushButton('Start Server')
-run_btn.clicked.connect(server)
+run_btn.clicked.connect(scotty.server)
 
 #kill server
 kill_btn = QPushButton('Kill Server')
-kill_btn.clicked.connect(kill_server)
+kill_btn.clicked.connect(scotty.kill_server)
 
 #build layout
 layout.addWidget(QLabel('Scotty - Simple HTTP File Server'))
